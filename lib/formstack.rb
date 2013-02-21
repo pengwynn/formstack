763c4872cc08579033b9af166d3c4a788dcc83fd
Joe Heth <joeheth@gmail.com>
2013-02-21 13:35:19 -0500

    Updating formstack.rb to be Ruby 1.9 compliant and removing explicit require for version.rb.

--- lib/formstack.rb
+++ lib/formstack.rb
@@ -2,24 +2,19 @@
 require "json"
 require "awesome_print"
 
-dir = Dir[File.dirname(__FILE__) + '/formstack']
+require "formstack/extensions/attr_accessor.rb"
 
-require "#{dir}/extensions/attr_accessor.rb"
+require "formstack/errors.rb"
+require "formstack/restful_resource.rb"
+require "formstack/form.rb"
 
-require "#{dir}/errors.rb"
-require "#{dir}/restful_resource.rb"
-require "#{dir}/form.rb"
-
-require "#{dir}/connection/http.rb"
-require "#{dir}/connection/oauth2_connection.rb"
-require "#{dir}/connection.rb"
+require "formstack/connection/http.rb"
+require "formstack/connection/oauth2_connection.rb"
+require "formstack/connection.rb"
 
 
 # some reason, FormStack::Form::CONTROLLER isn't generated
 FormStack::Form.const_set :CONTROLLER, "form" if !FormStack::Form.const_defined?(:CONTROLLER)
-
-
-require "#{dir}/version.rb"
 
 module FormStack
 
